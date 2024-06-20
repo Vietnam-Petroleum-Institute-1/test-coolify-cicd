0bcac5942404e7883be3929752bb1d6f5ebbe7fe
adminoilgasai <110379580+adminoilgasai@users.noreply.github.com>
2024-06-20 13:42:35 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-MY_VARI = $API_KEY
-
+import os
 app = FastAPI()
 
 @app.get("/helloworld")
 async def hello_world():
     return {"message": "Hello World"}
-
+    
+MY_VARI = os.environ.get("API_KEY", "")
 @app.get("/hello")
 async def hello():
     return {"message": "Hi, this is cicd" + MY_VARI}
