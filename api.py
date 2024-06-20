b6480cfe854ad863d407b069bd0ba6e861923c40
adminoilgasai <110379580+adminoilgasai@users.noreply.github.com>
2024-06-20 09:31:36 +0700

    Update api.py

--- api.py
+++ api.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/helloworld")
 async def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/hello")
+async def hello():
+    return {"message": "Hi, this is cicd"}
