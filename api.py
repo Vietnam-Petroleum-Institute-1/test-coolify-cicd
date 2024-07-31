c6c15894b8434f9c42b95eaccc7d43ac6861103e
sonnt-dna <110382822+sonnt-dna@users.noreply.github.com>
2024-07-31 16:59:40 +0700

    Update api.py

--- api.py
+++ api.py
@@ -149,4 +149,4 @@ async def train(file: UploadFile = File(...), feature_cols: str = Form(...)):
 # Run the app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
